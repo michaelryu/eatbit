a3377c86c2e709ec2b45483be900aad82cb081a3
Michael Ryu <michaelryu0@gmail.com>
2015-07-15 12:01:03 -0700

    added user creation through sms

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -3,8 +3,13 @@ class TextsController < ApplicationController
   skip_before_filter :force_ssl
 
   def create
-    @text = [params['From'], params['Body'], params['MediaUrl0'], params['MediaUrl1'], params['MediaUrl2']]
-    TextMailer.outbound(@text).deliver_now
-    render xml: '<Response/>'
+    # @text = [params['From'], params['Body'], params['MediaUrl0'], params['MediaUrl1'], params['MediaUrl2']]
+    # TextMailer.outbound(@text).deliver_now
+    # render xml: '<Response/>'
+    @user = User.find(phone: params['From'])
+    rescue ActiveRecord::RecordNotFound
+      @user = User.create
+      @user.update_attribute('phone', params['From'])
+      puts @user.phone
   end
 end
