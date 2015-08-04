c7b3a431714c75375f6391bac1fc36bd08c73a4d
Michael Ryu <michaelryu0@gmail.com>
2015-08-03 19:56:59 -0700

    added require

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -16,6 +16,7 @@ class TextsController < ApplicationController
     return unless params['MediaUrl0']
     link = process_uri(params['MediaUrl0'])
     img = MiniMagick::Image.open(link)
+    require 'zbar'
     data = ZBar::Image.from_pgm(img.format('PGM').to_blob).process
     code = data[0].instance_variable_get(:@data)
     product = Openfoodfacts::Product.get(code, locale: 'world')
