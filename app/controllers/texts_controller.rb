8535e79996efb55c4bb49a1008bcc96f0c0c34d2
Michael Ryu <michaelryu0@gmail.com>
2015-08-07 09:34:14 -0700

    changed message line to eatbit

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -39,7 +39,8 @@ class TextsController < ApplicationController
     return if @user
     @user = User.create(phone: params['From'], owner: params['To'])
     message(@user.phone, "Hi, welcome to Eatbit! Click to pay and enable your account:
-        https://count-calories.herokuapp.com/users/#{@user.id}", '415-592-6475')
+        http://x.eatbit.co/users/#{@user.id}
+        Secure payment by http://www.stripe.com Stripe", '415-592-6475')
     message(@user.phone, 'Questions? Just ask!', '415-592-6475')
   end
 
