16d72bce470b59341c75a9ae18e194257535a3f3
Michael Ryu <michaelryu0@gmail.com>
2015-08-10 10:10:32 -0700

    changed question line to secondary
    test
    
    :i
    
    t commit

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -37,7 +37,12 @@ class TextsController < ApplicationController
                               product['product']['nutriments']['energy'])
     else
       message(@user.phone,
+<<<<<<< HEAD
               'We could not find the product! Could you give us a short description of it?', '415-592-6475')
+=======
+              'We could not find the product!
+              Could you give us a short description of it?', '415-592-6475')
+>>>>>>> 4c58e0c... changed question line to secondary
     end
   end
 
